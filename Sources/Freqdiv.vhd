912b860f14004e45b4fc4ea2a6d81809aaf70ce9
ItnasMina <santiago.criadogonzalo@gmail.com>
2026-01-12 21:48:47 +0100

    Estructura inicial de Registo.vhd Modificaciones menores a Freqdiv.vhd

--- Sources/Freqdiv.vhd
+++ Sources/Freqdiv.vhd
@@ -7,29 +7,29 @@ entity Freqdiv is
     Port (clk    :  in  std_logic;
           reset  :  in  std_logic;
           
-          freqdiv_out :  out std_logic);
+          fdiv_out :  out std_logic);
 end Freqdiv;
 
 architecture Behavioral of Freqdiv is
 
-    constant max_count : integer := 5*125000000/1000000 -1;
-    --constant max_count : integer := 20; -- Para simulación rápida
-    signal counter : integer range 0 to max_count := 0 ;
+    --constant max_count : integer := 5*125000000/1000000 -1;
+    constant max_count : integer := 20; -- Para simulación rápida
+    signal count : integer range 0 to max_count := 0 ;
 
 begin
 
     process(clk, reset)
     begin
         if reset = '1' then
-            counter <= 0;
-            freqdiv_out <= '0';
+            count <= 0;
+            fdiv_out <= '0';
         elsif clk'event and clk = '1' then
-            if counter < max_count -1 then
-                counter <= counter + 1;
-                freqdiv_out <= '0';
+            if count < max_count -1 then
+                count <= count + 1;
+                fdiv_out <= '0';
             else
-                counter <= 0;
-                freqdiv_out <= '1';
+                count <= 0;
+                fdiv_out <= '1';
             end if;
         end if;
     end process;
---
+++ Sources/Registro.vhd
@@ -0,0 +1,39 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity Registro is
+    Port (clk : in STD_LOGIC;
+          reset : in STD_LOGIC;
+
+
+
+          NumPiedras1_in : in std_logic_vector (1 downto 0);
+          NumPiedras2_in : in std_logic_vector (1 downto 0);
+          NumPiedras3_in : in std_logic_vector (1 downto 0);
+          NumPiedras4_in : in std_logic_vector (1 downto 0);
+
+          NumPiedras1_out : out std_logic_vector (1 downto 0);
+          NumPiedras2_out : out std_logic_vector (1 downto 0);
+          NumPiedras3_out : out std_logic_vector (1 downto 0);
+          NumPiedras4_out : out std_logic_vector (1 downto 0);
+
+          Apuesta1_in : in std_logic_vector (3 downto 0);
+          Apuesta2_in : in std_logic_vector (3 downto 0);
+          Apuesta3_in : in std_logic_vector (3 downto 0);
+          Apuesta4_in : in std_logic_vector (3 downto 0);
+
+          Apuesta1_out : out std_logic_vector (3 downto 0);
+          Apuesta2_out : out std_logic_vector (3 downto 0);
+          Apuesta3_out : out std_logic_vector (3 downto 0);
+          Apuesta4_out : out std_logic_vector (3 downto 0);
+
+          Puntos1_in : in std_logic;
+          Puntos2_in : in std_logic;
+          Puntos3_in : in std_logic;
+          Puntos4_in : in std_logic;
+
+          Puntos1_out : out std_logic;
+          Puntos2_out : out std_logic;
+          Puntos3_out : out std_logic;
+          Puntos4_out : out std_logic);
+end entity;
